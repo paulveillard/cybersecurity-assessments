2dbf777df193cb0674e8651dd0c8f12c33702e02
Paul Veillard, M. Eng, CISM, CISA, CRISC <36570260+paulveillard@users.noreply.github.com>
2025-02-15 09:25:53 -0500

    Delete 7 - caiq-ccm directory

--- 7 - caiq-ccm/csa-ccm-tools/lib/csa/ccm/cli/version.rb
+++
@@ -1,7 +0,0 @@
-module Csa
-  module Ccm
-  	module Cli
-      VERSION = "0.1.4"
-    end
-  end
-end
--- 7 - caiq-ccm/csa-ccm-tools/lib/ext/string.rb
+++
@@ -1,7 +0,0 @@
-class String
-  def to_xls_col
-    return -1 if self.length > 1
-
-    self.upcase[0].ord - 'A'.ord
-  end
-end
